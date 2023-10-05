f2d89c6db49eae71a1fc97554540cfe68b7bb72e
LizBarrios <141792789+LizBarrios@users.noreply.github.com>
2023-10-04 20:30:19 -0700

    modified:   examen/.vscode/launch.json 	modified:   examen/examen.c 	modified:   examen/examen.exe 	modified:   taller 3 (modified content, untracked content)

--- examen/examen.c
+++ examen/examen.c
@@ -1,25 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-int validarNumeros(char mensaje[], int ri);
+int validarNumeros(char mensaje[], int ri, int rf);
+int repetir();
 
 int main()
 {
-    int num, i, suma = 0, bandera, j;
-    num = validarNumeros("ingrese un número entero positivo para dar su número perfecto ", 0);
-    for (i = 0; i < num; i++)
+    int num, i, suma, bandera, j, k, bandera;
+    do
     {
-        for (j = 1; j < i; j++)
+        bandera = 0;
+        num = validarNumeros("ingrese un número entero positivo para dar su número perfecto ", 0, 1000000000);
+        for (i = 1; i <= num; i++)
         {
-            if (i % j == 0)
+            suma = 0;
+            for (j = 1; j < i; j++)
             {
-                suma += j;
+                if (i % j == 0)
+                {
+                    suma += j;
+                }
+            }
+            if (suma == i)
+            {
+                printf("el numero perfecto es:%d =", i);
+                for (k = 1; k < i; k++)
+                {
+                    if (i % k == 0)
+                    {
+                        printf("%d ", k);
+                    }
+                }
+                printf("\n");
             }
         }
-    }
-    return 0;
+        bandera = repetir();
+
+    } while (bandera == 1);
 }
 
-int validarNumeros(char mensaje[], int ri)
+int repetir()
+{
+    int repetir;
+    repetir = validarNumeros("QUIERES HACER OTRA OPCION\nSI=1\nNO=2\n", 1, 2);
+    if (repetir == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        printf("FIN DEL POGRAMA");
+        return 0;
+    }
+}
+int validarNumeros(char mensaje[], int ri, int rf)
 {
     char entrada[100];
     int num;
@@ -28,11 +61,10 @@ int validarNumeros(char mensaje[], int ri)
         printf("%s", mensaje);
         fgets(entrada, sizeof(entrada), stdin);
         num = atoi(entrada);
-        if (num < ri && num == 1)
+        if (num < ri || num > rf)
         {
             printf("INGRESE EL NUMERO OTRA VEZ PORFAVOR\n");
         }
-    } while (num < ri);
-
+    } while (num < ri || num > rf);
     return num;
 }
